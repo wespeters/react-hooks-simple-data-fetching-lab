34691635863f095eb50e3f60a181d0223090041c
Wesley Peters <wesleypeters@gmail.com>
2023-06-17 07:43:48 -0600

    add fetch request and loading indicator

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
-// create your App component here
+import React, { useEffect, useState } from 'react';
+
+const App = () => {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(res => res.json())
+      .then(data => setDogImage(data.message));
+  }, []);
+
+  return (
+    <div>
+      {dogImage ? (
+        <img src={dogImage} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default App;
